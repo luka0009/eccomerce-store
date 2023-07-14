95b434413862ceb30b69e1c472334af21df2e3a0
luka <lukabarbakadze18@gmail.com>
2023-07-14 17:08:21 +0400

    individual product page

--- actions/get-products.tsx
+++ actions/get-products.tsx
@@ -10,20 +10,22 @@ interface Query {
 	isFeatured?: boolean;
 }
 
-const getProducts = async (query: Query): Promise<Product[]> => {
+const getProducts = async (query: Query, excludeProductId?: string): Promise<Product[]> => {
 	const url = qs.stringifyUrl({
-		url: URL,
-		query: {
-			colorId: query.colorId,
-			sizeId: query.sizeId,
-			categoryId: query.categoryId,
-			isFeatured: query.isFeatured,
-		},
+	  url: URL,
+	  query: {
+		colorId: query.colorId,
+		sizeId: query.sizeId,
+		categoryId: query.categoryId,
+		isFeatured: query.isFeatured,
+	  },
 	});
-
-	const res = await fetch(URL);
-
-	return res.json();
-};
-
+  
+	const res = await fetch(url);
+	const products: Product[] = await res.json();
+  
+	const filteredProducts = products.filter((product) => product.id !== excludeProductId);
+  
+	return filteredProducts;
+  };
 export default getProducts;
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -6,14 +6,24 @@ import Image from "next/image";
 import IconButton from "./icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "./currency";
+import { useRouter } from "next/navigation";
 
 interface ProductCardProps {
 	data: Product;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+	const router = useRouter();
+
+	const handleClick = () => {
+		router.push(`/product/${data?.id}`);
+	};
+
 	return (
-		<div className="bg-white group cursor-pointer rounded-xl border space-y-4 pb-2">
+		<div
+			onClick={handleClick}
+			className="bg-white group cursor-pointer rounded-xl border space-y-4 pb-2"
+		>
 			<div className="aspect-square rounded-xl bg-gray-100 relative">
 				<Image
 					src={data?.images?.[0].url}
@@ -42,7 +52,6 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
 			<div className="flex items-center justify-between px-2">
 				<Currency value={data?.price} />
 			</div>
-
 		</div>
 	);
 };
